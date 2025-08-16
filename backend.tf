230fa85053a1701baa19134b85924cbc592d185c
okwarajustin <jjustinbrowno@gmail.com>
2025-08-16 13:04:54 -0400

    Update

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
 
-# terraform {
-#   backend "azurerm" {
-#     storage_account_name = "mystorageforbackend"
-#     resource_group_name  = "terraform-rg"      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-#     container_name       = "mycontainer"       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-#     key                  = "terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    storage_account_name = "mystorageforbackend123"
+    subscription_id      = "4f280b52-14a6-4c07-9f6f-2b41c444db28"
+    resource_group_name  = "RG"      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "mycontainer"       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+}
